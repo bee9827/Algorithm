b7ceebc3859eccdb1757fde313e6aef455b85079
bee9827 <ehfrhfo9494@naver.com>
2025-06-14 21:59:53 +0900

    2 / S2 / BruteForce
    시간복잡도: O(N^N) -> ???
    2<=n<=10
    
    이 문제는 특이한게 O(N^N)으로 구현했는데 잘 작동한다.
    처음에는 BruteForce에 DFS를 사용한다면 이 문제가 시간초과가 날것이라고 생각했지만 의외로 시간초과가 나지 않았다.
    그래서 next_permutation을 이용한다면 O(N!)으로 구현할 수 있지만 구현하는데 시간이 많이 든다고 생각해서 구현하지 않았다.
    
    1. 순회조건을 생각하지 않아 마지막 depth에서 start 노드로 갈 수 있는지 체킹을 하지 않았다.

---
+++ src/week5/외판원순회2_S2_10971.java
@@ -0,0 +1,84 @@
+package week5;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+
+ */
+
+public class 외판원순회2_S2_10971 {
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[][] edge;
+
+    int t, n, m;
+
+
+    long ans;
+
+    public static void main(String[] args) throws IOException {
+        외판원순회2_S2_10971 solve = new 외판원순회2_S2_10971();
+        solve.init();
+    }
+
+    public void init() throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    public StringTokenizer readLine() {
+        try {
+            return new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+            throw new IllegalArgumentException("읽어오지 못했습니다.");
+        }
+    }
+
+    public void input() {
+        n = Integer.parseInt(readLine().nextToken());
+
+        edge = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = readLine();
+            for (int j = 0; j < n; j++) {
+                edge[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    private void output() throws IOException {
+        bw.flush();
+        bw.close();
+    }
+
+    public void solve() throws IOException {
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+        ans = Integer.MAX_VALUE;
+        dfs(edge, visited, 0, 0, 0, 0);
+        bw.write(String.valueOf(ans));
+    }
+
+    public void dfs(int[][] edge, boolean[] visited, int start, int curr, int depth, int cost) {
+        if (depth == n - 1) {
+            if (edge[curr][start] != 0) ans = Math.min(ans, cost + edge[curr][start]);
+            return;
+        }
+
+        for (int nextNode = 0; nextNode < n; nextNode++) {
+            if (edge[curr][nextNode] == 0 || visited[nextNode]) continue;
+
+            visited[nextNode] = true;
+            dfs(edge, visited, start, nextNode, depth + 1, cost + edge[curr][nextNode]);
+            visited[nextNode] = false;
+        }
+
+
+    }
+
+
+}
